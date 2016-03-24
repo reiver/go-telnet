cc9c08d26ee257e9708345cf25007c2739532847
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-03-24 11:15:39 -0700

    added tests

--- telsh/telnet_handler_test.go
+++ telsh/telnet_handler_test.go
@@ -2,10 +2,107 @@ package telsh
 
 
 import (
+	"bytes"
+	"strings"
+
 	"testing"
 )
 
 
-func TestServeTELNET(t *testing.T) {
-t.Errorf("TODO")
+func TestServeTELNETCommandNotFound(t *testing.T) {
+
+	tests := []struct{
+		ClientSends string
+		Expected   string
+	}{
+		{
+			ClientSends: "\r\n",
+			Expected: "",
+		},
+
+
+
+		{
+			ClientSends: "apple\r\n",
+			Expected:    "apple: command not found\r\n",
+		},
+		{
+			ClientSends: "banana\r\n",
+			Expected:    "banana: command not found\r\n",
+		},
+		{
+			ClientSends: "cherry\r\n",
+			Expected:    "cherry: command not found\r\n",
+		},
+
+
+
+		{
+			ClientSends: "\t\r\n",
+			Expected: "",
+		},
+		{
+			ClientSends: "\t\t\r\n",
+			Expected: "",
+		},
+		{
+			ClientSends: "\t\t\t\r\n",
+			Expected: "",
+		},
+
+
+
+		{
+			ClientSends: " \r\n",
+			Expected: "",
+		},
+		{
+			ClientSends: "  \r\n",
+			Expected: "",
+		},
+		{
+			ClientSends: "   \r\n",
+			Expected: "",
+		},
+
+
+
+		{
+			ClientSends: " \t\r\n",
+			Expected: "",
+		},
+		{
+			ClientSends: "\t \r\n",
+			Expected: "",
+		},
+
+
+
+		{
+			ClientSends: "ls -alF\r\n",
+			Expected:    "ls: command not found\r\n",
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		shellHandler := NewShellHandler()
+		if nil == shellHandler {
+			t.Errorf("For test #%d, did not expect to get nil, but actually got it: %v; for client sent: %q", testNumber, shellHandler, test.ClientSends)
+			continue
+		}
+
+
+		var ctx Context
+
+		var buffer bytes.Buffer
+
+		shellHandler.ServeTELNET(ctx, &buffer, strings.NewReader(test.ClientSends))
+
+		if expected, actual := shellHandler.WelcomeMessage+shellHandler.Prompt+test.Expected+shellHandler.Prompt+shellHandler.ExitMessage, buffer.String(); expected != actual {
+			t.Errorf("For test #%d, expect %q, but actually got %q; for client sent: %q", testNumber, expected, actual, test.ClientSends)
+			continue
+		}
+	}
 }
