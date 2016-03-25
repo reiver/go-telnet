d210aaf14c4a3cbeb8a4fdfb219ead27b50cc0ca
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-03-25 13:47:41 -0700

    added more docs

--- doc.go
+++ doc.go
@@ -20,7 +20,7 @@ ListenAndServe starts a TELNET server with a given address and handler.
 Example TELNETS Server
 
 ListenAndServeTLS starts a (secure) TELNETS server with a given address and handler,
-using the specific "cert.pem" and "key.pem" files.
+using the specified "cert.pem" and "key.pem" files.
 
 	handler := telnet.EchoHandler
 	
@@ -38,6 +38,134 @@ If you are communicating just on localhost, then using just (the un-secure) TELN
 
 If you are not sure which to use, use TELNETS and ListenAndServeTLS.
 
+
+Example TELNET Shell Server
+
+The previous 2 exaple servers were very very simple. Specifically, they just echoed back whatever
+you submitted to it.
+
+If you typed:
+
+	Apple Banana Cherry\r\n
+
+... it would send back:
+
+	Apple Banana Cherry\r\n
+
+(Exactly the same data you sent it.)
+
+A more useful TELNET server can be made using the "github.com/reiver/go-telnet/telsh" sub-package.
+
+The `telsh` sub-package provides "middleware" that enables you to create a "shell" interface (also
+called a "command line interface" or "CLI") which most people would expect when using TELNET OR TELNETS.
+
+For example:
+
+
+	package main
+	
+	
+	import (
+		"github.com/reiver/go-oi"
+		"github.com/reiver/go-telnet"
+		"github.com/reiver/go-telnet/telsh"
+		
+		"time"
+	)
+	
+	
+	func main() {
+		
+		shellHandler := telsh.NewShellHandler()
+
+		commandName := "date"
+		shellHandler.Register(commandName, danceProducer)
+
+		commandName = "animate"
+		shellHandler.Register(commandName, animateProducer)
+		
+		addr := ":23"
+		if err := telnet.ListenAndServe(addr, shellHandler); nil != err {
+			panic(err)
+		}
+	}
+
+Note that in the example, so far, we have registered 2 commands: `date` and `animate`.
+
+For this to actually work, we need to have code for the `date` and `animate` commands.
+
+The actual implemenation for the `date` command could be done like the following:
+
+	func dateHandlerFunc(stdin io.ReadCloser, stdout io.WriteCloser, stderr io.WriteCloser) error {
+		const layout = "Mon Jan 2 15:04:05 -0700 MST 2006"
+		s := time.Now().Format(layout)
+		
+		if _, err := oi.LongWriteString(stdout, s); nil != err {
+			return err
+		}
+		
+		return nil
+	}
+	
+	
+	func dateProducerFunc(ctx telsh.Context, name string, args ...string) telsh.Handler{
+		return telsh.PromoteHandlerFunc(dateHandler)
+	}
+	
+	
+	var dateProducer = ProducerFunc(dateProducerFunc)
+
+Note that your "real" work is in the `dateHandlerFunc` func.
+
+And the actual implementation for the `animate` command could as done as follows:
+
+	func animateHandlerFunc(stdin io.ReadCloser, stdout io.WriteCloser, stderr io.WriteCloser) error {
+		
+		for i:=0; i<20; i++ {
+			oi.LongWriteString(stdout, "\r⠋")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠙")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠹")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠸")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠼")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠴")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠦")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠧")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠇")
+			time.Sleep(50*time.Millisecond)
+			
+			oi.LongWriteString(stdout, "\r⠏")
+			time.Sleep(50*time.Millisecond)
+		}
+		oi.LongWriteString(stdout, "\r \r\n")
+		
+		return nil
+	}
+	
+	
+	func animateProducerFunc(ctx telsh.Context, name string, args ...string) telsh.Handler{
+		return telsh.PromoteHandlerFunc(animateHandler)
+	}
+	
+	
+	var animateProducer = ProducerFunc(animateProducerFunc)
+
+Again, note that your "real" work is in the `animateHandlerFunc` func.
 
 Generating PEM Files
 
