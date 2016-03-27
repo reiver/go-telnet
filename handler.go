ba4ec1ee188abd4456779fb4de60a81f52282c47
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-03-27 11:29:19 -0700

    added more content to docs

--- handler.go
+++ handler.go
@@ -6,22 +6,22 @@ import (
 )
 
 
-// A Handler serves a TELNET connection.
+// A Handler serves a TELNET (or TELNETS) connection.
 //
 // Writing data to the io.Writer passed as an argument the ServeTELNET method
-// will send data to the TELNET client.
+// will send data to the TELNET (or TELNETS) client.
 //
-// Reading data from the io.Reader passed as an argument the ServeTELNET method
-// will receive data from the TELNET client.
+// Reading data from the io.Reader passed as an argument to the ServeTELNET method
+// will receive data from the TELNET (or TELNETS) client.
 //
 // The Reader's Read method and Writer's Write method receive and send
-// the raw TELNET communications!
+// the raw TELNET (or TELNETS) communications!
 //
 // Meaning, for example, that it includes commands and escaping.
 //
-// You could use DataReader to un-escape raw TELNET communications.
+// You can use DataReader to un-escape raw TELNET (or TELNETS) communications.
 //
-// And you could use DataWriter to escape into raw TELNET communications.
+// And you can use DataWriter to escape into raw TELNET (or TELNETS) communications.
 type Handler interface {
 	ServeTELNET(Context, io.Writer, io.Reader)
 }
