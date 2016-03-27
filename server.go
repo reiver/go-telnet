e982b30b1af437f2249727ac97533a9fc6c9c5ae
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-03-27 15:53:28 -0700

    logger

--- server.go
+++ server.go
@@ -74,8 +74,9 @@ type Server struct {
 	Handler Handler // handler to invoke; telnet.EchoServer if nil
 
 	TLSConfig *tls.Config // optional TLS configuration; used by ListenAndServeTLS.
-}
 
+	Logger Logger
+}
 
 // ListenAndServe listens on the TCP network address 'server.Addr' and then spawns a call to the ServeTELNET
 // method on the 'server.Handler' to serve each incoming connection.
@@ -127,27 +128,33 @@ func (server *Server) Serve(listener net.Listener) error {
 	defer listener.Close()
 
 
+	logger := server.logger()
+
+
 	handler := server.Handler
 	if nil == handler {
 //@TODO: Should this be a "ShellHandler" instead, that gives a shell-like experience by default
 //       If this is changd, then need to change the comment in the "type Server struct" definition.
+		logger.Debug("Defaulted handler to EchoHandler.")
 		handler = EchoHandler
 	}
 
 
 	for {
 		// Wait for a new TELNET client connection.
+		logger.Debugf("Listening at %q.", listener.Addr())
 		conn, err := listener.Accept()
 		if err != nil {
 //@TODO: Could try to recover from certain kinds of errors. Maybe waiting a while before trying again.
 			return err
 		}
+		logger.Debugf("Received new connection from %q.", conn.RemoteAddr())
 
 		// Handle the new TELNET client connection by spawning
 		// a new goroutine.
 		go func(c net.Conn) {
 //@TODO: Add proper context.
-			var ctx Context = nil
+			var ctx Context = NewContext().InjectLogger(logger)
 
 			var w io.Writer = c
 			var r io.Reader = c
@@ -155,5 +162,16 @@ func (server *Server) Serve(listener net.Listener) error {
 			handler.ServeTELNET(ctx, w, r)
 			c.Close()
 		}(conn)
+		logger.Debugf("Spawned handler to handle connection from %q.", conn.RemoteAddr())
+        }
+}
+
+
+func (server *Server) logger() Logger {
+	logger := server.Logger
+	if nil == logger {
+		logger = internalDiscardLogger{}
 	}
+
+	return logger
 }
